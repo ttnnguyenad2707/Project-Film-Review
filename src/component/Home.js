4d19a4e39f967e75d0cbc23f06e52d2d17a56599
put1nn <haitghe163706@fpt.edu.vn>
2023-03-22 18:44:43 +0700

    Search + List category

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,10 +3,32 @@ import { Link } from "react-router-dom";
 import Category from '../Json/Movie.json'
 
 export default function Home() {
-    
+
     const [category, setCategory] = useState(Category)
     //load movie
     const [categoryLoad, setCategoryLoad] = useState(Category)
+    const [showSearch, setShowSearch] = useState(false)
+    const [movie, setMovie] = useState([])
+
+    const handleSearch = (search) => {
+
+        if (search == "") {
+            setShowSearch(false)
+        }
+        setShowSearch(true)
+        var searchList = [];
+        category.map(category => {
+            var temp = category.Movie.filter(movie => movie.Name.toLowerCase().includes(search.toLowerCase()))
+            temp = temp.map(movie => {
+                return { ...movie, Category: category.Name }
+            })
+            if (temp.length !== 0) {
+                searchList.push(...temp)
+            }
+        })
+        setMovie(searchList);
+    }
+
 
 
     return (
@@ -21,61 +43,69 @@ export default function Home() {
                     <Link to="/Register" >Đăng kí </Link>
                 </div>
                 <form class="form-inline">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <input onChange={e => handleSearch(e.target.value)} class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-
                 </form>
-
             </nav>
-
             <div class="row">
                 <div className="col-3">
                     <h1 style={{ color: "red" }} > Thể loại </h1> <hr />
-                </div>
-
-                <div className="col-9">
-
-                    <h1 style={{ color: "red" }}> Danh sách phim</h1> <hr />
-
-
-                    <div class="row">  {categoryLoad.map((category) => (
+                    {categoryLoad.map(cat =>
                         <Fragment>
-                            {
-                                category.Movie.map((movie) => (<div class="card col-4" >
-                                    <div class="card-body ">
-                                        <div class="">
-                                            <div>
-                                                <div>Tên phim: {movie.Name} </div>
+                            <a style={{color: "red"}} href="#">{cat.Name}</a>
+                            <br></br>
+                        </Fragment>
+                    )}
+                </div>
+                <div className="col-9">
+                    <h1 style={{ color: "red" }}> Danh sách phim</h1> <hr />
+                    <div class="row">
+                        {showSearch == true && movie.map(movie => (
+                            <div class="card col-4" >
+                                <div class="card-body ">
+                                    <div class="">
+                                        <div>
+                                            <div>Tên phim: {movie.Name} </div>
 
-                                                <img src={movie.Thumbnail} />
-                                                <div> Mô tả: {movie.Description} </div>
+                                            <img src={movie.Thumbnail} />
+                                            <div> Mô tả: {movie.Description} </div>
 
-                                                <div>{category.Name} </div>
-                                                <div>Đánh giá: {movie.AvgRating} </div>
-                                                <button class="btn btn-outline-primary">đánh giá</button>
+                                            <div>{category.Name} </div>
+                                            <div>Đánh giá: {movie.AvgRating} </div>
+                                            <button class="btn btn-outline-primary">đánh giá</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                        {showSearch == false && categoryLoad.map((category) => (
+                            <Fragment>
+                                {
+                                    category.Movie.map((movie) => (
+                                        <div class="card col-4" >
+                                            <div class="card-body ">
+                                                <div class="">
+                                                    <div>
+                                                        <div>Tên phim: {movie.Name} </div>
+
+                                                        <img src={movie.Thumbnail} />
+                                                        <div> Mô tả: {movie.Description} </div>
+
+                                                        <div>{category.Name} </div>
+                                                        <div>Đánh giá: {movie.AvgRating} </div>
+                                                        <button class="btn btn-outline-primary">đánh giá</button>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
 
-
-                                    </div>
-                                </div>
-
-                                ))
-                            }
-                        </Fragment>
-
-
-                    ))}
+                                    ))
+                                }
+                            </Fragment>
+                        ))}
                     </div>
-
                 </div>
-
             </div>
-
-
-
-
-
         </div>
 
     )
